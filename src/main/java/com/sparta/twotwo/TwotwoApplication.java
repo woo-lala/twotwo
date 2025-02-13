fadc99f7a9e2380f0be0bf5876e592865b1f6d15
woo-lala <hyunj2034@naver.com>
2025-02-13 11:35:42 +0900

    init project

---
+++ src/main/java/com/sparta/twotwo/TwotwoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.twotwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwotwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwotwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/twotwo/TwotwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.twotwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwotwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
