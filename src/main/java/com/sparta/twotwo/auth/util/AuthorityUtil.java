17efc12383be9869f2b49148e79e7ad0ba1cd6c5
earthkid0 <ghs1541@naver.com>
2025-02-15 19:58:22 +0900

    OWNER 권한 임시 설정

--- src/main/java/com/sparta/twotwo/auth/util/AuthorityUtil.java
+++ src/main/java/com/sparta/twotwo/auth/util/AuthorityUtil.java
@@ -8,6 +8,8 @@ import java.util.List;
 public class AuthorityUtil {
     private final static String masterEmail = "master@email.com";
     private final static String managerEmail = "manager@email.com";
+    private final static String ownerEmail = "owner@email.com";
+
 
     private final String CUSTOMER = RolesEnum.CUSTOMER.getAuthority();
     private final List<String> MASTER_STRING = List.of(RolesEnum.MASTER.getAuthority(), CUSTOMER);
@@ -20,6 +22,8 @@ public class AuthorityUtil {
             return MASTER_STRING;
         } else if(email.equals(managerEmail)) {
             return MANAGER_STRING;
+        } else if(email.equals(ownerEmail)) { // 테스트를 위한 owner 권한 설정 (이메일말고 다른 검증으로 권한 부여 예정)
+            return OWNER_STRING;
         }
         return CUSTOMER_STRING;
     }
