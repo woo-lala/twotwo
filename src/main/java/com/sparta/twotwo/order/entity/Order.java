4684b7387a53cccce61c01b2821aba1e263dd36e
k_dh610 <k_dh610@naver.com>
2025-02-17 10:59:05 +0900

    refactor: Order Entity Builder패턴 수정

--- src/main/java/com/sparta/twotwo/order/entity/Order.java
+++ src/main/java/com/sparta/twotwo/order/entity/Order.java
@@ -4,21 +4,23 @@ import com.sparta.twotwo.common.auditing.BaseEntity;
 import com.sparta.twotwo.enums.OrderType;
 import com.sparta.twotwo.order.dto.OrderResponseDto;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import java.util.UUID;
 
 @Entity
 @Getter
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "p_order")
 public class Order extends BaseEntity {
+
+    @Builder
+    public Order(OrderType order_type, Long price) {
+        this.order_type = order_type;
+        this.price = price;
+    }
 
     @Id
     @GeneratedValue(generator = "uuid2")
@@ -26,11 +28,21 @@ public class Order extends BaseEntity {
     @Column(columnDefinition = "BINARY(16)")
     private UUID order_id;
 
+    @Enumerated
     @Column(name = "order_type")
     private OrderType order_type;
 
     @Column(name = "price")
     private Long price;
+
+    public void changePrice(Long price) {
+        this.price = price;
+    }
+
+    public void changeOrderType(OrderType order_type) {
+        this.order_type = order_type;
+    }
+
 
     public OrderResponseDto toResponseDto() {
         return OrderResponseDto.builder()
--- src/test/java/com/sparta/twotwo/order/entity/OrderTest.java
+++ src/test/java/com/sparta/twotwo/order/entity/OrderTest.java
@@ -7,6 +7,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,16 +23,52 @@ class OrderTest {
 
     @Test
     @DisplayName("주문 생성")
+    @Transactional
     public void save(){
 
         Order order = Order.builder()
                 .order_type(OrderType.ONLINE)
-                .price(10000L)
+                .price(20000L)
                 .build();
 
         repository.save(order);
 
+//        Assertions.assertThat(repository.findAll()).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("주문 삭제")
+    @Transactional
+    public void delete(){
+
+        Order order = Order.builder()
+                .order_type(OrderType.ONLINE)
+                .price(1000L)
+                .build();
+        repository.save(order);
+        repository.delete(order);
         Assertions.assertThat(repository.findAll()).hasSize(1);
+
+    }
+
+    @Test
+    @DisplayName("주문 수정")
+//    @Transactional
+    public void update(){
+        Order order = Order.builder()
+                .order_type(OrderType.ONLINE)
+                .price(1000L)
+                .build();
+        repository.save(order);
+
+        Order find = repository.findById(order.getOrder_id()).orElseThrow(NullPointerException::new);
+
+        find.changePrice(2000L);
+        repository.save(find);
+
+        Order update = repository.findById(find.getOrder_id()).orElseThrow(NullPointerException::new);
+        Assertions.assertThat(update.getPrice()).isEqualTo(2000L);
+
 
     }
 
