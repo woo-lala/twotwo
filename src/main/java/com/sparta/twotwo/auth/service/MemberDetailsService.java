95b341a382cbfe422a1304920acfc8c02a03674b
earthkid0 <ghs1541@naver.com>
2025-02-18 15:18:10 +0900

    style/import 문 optimizer

--- src/main/java/com/sparta/twotwo/auth/service/MemberDetailsService.java
+++ src/main/java/com/sparta/twotwo/auth/service/MemberDetailsService.java
@@ -1,16 +1,13 @@
 package com.sparta.twotwo.auth.service;
 
-import com.sparta.twotwo.auth.util.AuthorityUtil;
 import com.sparta.twotwo.members.entity.Member;
 import com.sparta.twotwo.members.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @Service
