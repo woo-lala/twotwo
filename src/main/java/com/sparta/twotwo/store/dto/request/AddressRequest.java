8be92f28087df8a93682021729eae68f43f803c5
woo-lala <hyunj2034@naver.com>
2025-02-19 15:51:04 +0900

    refactor: Store, Request, Response 수정

--- src/main/java/com/sparta/twotwo/store/dto/request/AddressRequest.java
+++ src/main/java/com/sparta/twotwo/store/dto/request/AddressRequest.java
@@ -1,9 +1,10 @@
 package com.sparta.twotwo.store.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.UUID;
 
 @Getter
 @RequiredArgsConstructor
@@ -18,17 +19,16 @@ public class AddressRequest {
     @NotBlank(message = "읍면동을 작성해주세요.")
     private final String emd;
 
-    @NotBlank(message = "우편번호를 작성해주세요.")
+    @NotNull(message = "행정구역 코드를 작성해주세요.")
     private final String admCode;
 
-    @NotBlank(message = "우편번호를 작성해주세요.")
+    @NotNull(message = "우편번호를 작성해주세요.")
     private final String zipNum;
 
-    @NotBlank(message = "도로명주소를 작성해주세요.")
+    @NotNull(message = "도로명주소를 작성해주세요.")
     private final String roadAddress;
 
-    @NotBlank(message = "상세주소를 작성해주세요.")
+    @NotNull(message = "상세주소를 작성해주세요.")
     private final String detailAddress;
-
 
 }
--- src/main/java/com/sparta/twotwo/store/dto/request/StoreCreateRequest.java
+++ src/main/java/com/sparta/twotwo/store/dto/request/StoreCreateRequest.java
@@ -1,29 +1,22 @@
 package com.sparta.twotwo.store.dto.request;
 
-import com.sparta.twotwo.members.entity.Member;
-import com.sparta.twotwo.product.entity.Product;
-import com.sparta.twotwo.store.entity.Address;
-import com.sparta.twotwo.store.entity.StoreCategory;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.RequiredArgsConstructor;
+import lombok.Getter;
 
-import java.math.BigDecimal;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
+@Getter
 public class StoreCreateRequest {
 
-    @NotBlank(message = "가게명을 작성해주세요.")
+    @NotNull(message = "가게명을 작성해주세요.")
     @Size(max = 50, message = "가게명은 50자를 초과할 수 없습니다.")
     public final String name;
 
     @NotNull
-    private final UUID memberId;
+    private final Long memberId;
 
     @Valid
     private final AddressRequest address;
@@ -41,8 +34,7 @@ public class StoreCreateRequest {
     @NotNull(message = "운영 종료 시간을 작성해주세요.")
     private final LocalTime operationClosedAt;
 
-    public StoreCreateRequest(String name, UUID memberId, AddressRequest address, UUID categoryId, String imageUrl, LocalTime operationStartedAt, LocalTime operationClosedAt) {
-
+    public StoreCreateRequest(String name, Long memberId, AddressRequest address, UUID categoryId, String imageUrl, LocalTime operationStartedAt, LocalTime operationClosedAt) {
         this.name = name;
         this.memberId = memberId;
         this.address = address;
