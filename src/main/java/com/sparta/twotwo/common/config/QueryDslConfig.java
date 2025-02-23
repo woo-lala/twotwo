6455bd374ef0a91e811bb959c83fac0e2f401ebc
문고은 <>
2025-02-23 15:38:49 +0900

    fix: QueryDslConfig 클래스명 변경

--- src/main/java/com/sparta/twotwo/common/config/QueryDslConfig.java
+++ src/main/java/com/sparta/twotwo/common/config/QueryDslConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class QueryDSLConfig {
+public class QueryDslConfig {
     @PersistenceContext
     private EntityManager entityManager;
 
