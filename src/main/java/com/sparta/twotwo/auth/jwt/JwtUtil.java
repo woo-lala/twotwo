78cc5cb4038e810332f36f65886b54cdc816ed72
earthkid0 <ghs1541@naver.com>
2025-02-24 12:55:14 +0900

    refactor/jwt secretKey 랜덤키 생성에서 환경변수값으로 수정

--- src/main/java/com/sparta/twotwo/auth/jwt/JwtUtil.java
+++ src/main/java/com/sparta/twotwo/auth/jwt/JwtUtil.java
@@ -5,12 +5,17 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,16 +23,27 @@ import java.util.Map;
 @Component
 public class JwtUtil {
 
-    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
+//    private SecretKey key;
+//    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     @Getter
-    private final String secretKey = String.valueOf(key);
+    @Value("${jwt-secret-key}")
+    private String secretKey;
+
+//    @Getter
+//    private final String secretKey = String.valueOf(key);
 
     @Value("${expire-token-time}")
     int expireTokenTime;
 
+    public String encodeBase64SecretKey(String secretKey) {
+        return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
 
-    public String createToken(Member member) {
+
+
+    public String createToken(Member member, String base64EncodedSecretKey) {
+        Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
         Date date = new Date();
         Map<String, Object> claims = new HashMap<>();
         claims.put("member_id", member.getMember_id());
@@ -44,11 +60,19 @@ public class JwtUtil {
                 .compact();
     }
 
-    public Jws<Claims> getClaims(String token) {
-
+    public Jws<Claims> getClaims(String token, String base64EncodedSecretKey) {
+        Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
         return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()
                 .parseClaimsJws(token);
     }
+
+    private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(base64EncodedSecretKey);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+
+        return key;
+    }
+
 }
